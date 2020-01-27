dc467d2c1850fac673835f6b56d94b6c9cc144ee
heshamsayed <hesham.sayed636@gmail.com>
2020-01-27 14:15:04 +0200

    feat: Adding the views and urls and map the views to these urls for the api

--- api/music/views.py
+++ api/music/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Song
+from .serializers import SongSerializer
 
-# Create your views here.
+class ListSongView(generics.ListAPIView):
+    """
+    it provides a get method handler
+    """
+
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
